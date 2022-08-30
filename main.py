434cd53aa7e66958c42cd7dc2ad2ba36cf17fe33
Ingv4r <venom3800@yandex.ru>
2022-08-30 16:26:29 +0300

    Сreated the basis of the game

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-print('hi')
-dasdf
+import pygame as pg
+from settings import *
+from player import Player
+import math
+
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+clock = pg.time.Clock()
+player = Player()
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+            exit()
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                running = False
+                exit()
+    player.movement()
+    screen.fill(BLACK)
+
+    pg.draw.circle(screen, GREEN, player.pos, 8)
+    pg.draw.line(
+        screen, GREEN, player.pos, (player.x + WIDTH * math.cos(player.angle),
+                                    player.y + WIDTH * math.sin(player.angle))
+    )
+
+
+    pg.display.flip()
+    clock.tick(FPS)
---
+++ settings.py
@@ -0,0 +1,22 @@
+# game settings
+WIDTH = 800
+HEIGHT = 600
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+running = True
+FPS = 60
+
+#player setting
+player_pos = (HALF_WIDTH, HALF_HEIGHT)
+player_angle = 0
+player_speed = 2
+
+#colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (220, 0, 0)
+GREEN = (0, 220, 0)
+BLUE = (0, 0, 220)
+DARKGREY = (110, 110, 110)
+PURPLE = (120, 0, 120)
+
